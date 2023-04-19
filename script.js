ebda858810fea79164126059a4473b55d3affc40
woLy2005 <125599066+woLy2005@users.noreply.github.com>
2023-04-20 01:20:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true, // показать стрелки
+        dots:false, // не показывать точки
+        slidesToShow:3, // показывать по 3 слайда
+        autoplay:true, // автоматическое проигрывание слайдов
+    });
+});
